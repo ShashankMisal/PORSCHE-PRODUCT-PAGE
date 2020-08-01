5f32cdef6af30c56587f83d167e629e891d8605b
Shashank Misal <shaank1818@gmail.com>
2020-08-01 12:43:52 +0530

    ADDED ON OFF BUTTON FOR SOUND
    Removed Play pause ICONS

--- main/main.js
+++ main/main.js
@@ -1,9 +1,13 @@
 var sound = new Audio("main/AUDIO/aud1.mp3");
 
 function playAudio(){    
-    sound.play();   
+    sound.play(); 
+    document.getElementById("aud_pause").style.visibility="hidden";  
+    document.getElementById("aud_play").style.visibility="visible";  
 }
 
 function pauseAudio(){
     sound.pause();
+    document.getElementById("aud_pause").style.visibility="visible";  
+    document.getElementById("aud_play").style.visibility="hidden";  
 }
